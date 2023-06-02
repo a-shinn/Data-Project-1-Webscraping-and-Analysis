6856f0f64783b92e7e857ad2c5405eb75e608d74
Austin Shinn <81658373+shinnsplints@users.noreply.github.com>
2023-06-02 16:24:54 -0500

    Delete ghibliwikiscrape.py

--- ghibliwikiscrape.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import bs4 as bs
-import requests
-
-tables = pd.read_html('https://en.wikipedia.org/wiki/Studio_Ghibli')
-print(tables[0])
-print()
-print(tables[1])
