749fb01bc93c828aed61dd9097b3e509beaf0dec
Austin S <austinshinn99@gmail.com>
2023-06-02 14:25:00 -0500

    commit

--- nge_script.py
+++ nge_script.py
@@ -3,22 +3,20 @@ import bs4 as bs
 import requests
 import re
 
-webpage = requests.get('https://www.animanga.com/scripts/textesgb/eva1.html')
+webpage = requests.get('https://www.animanga.com/scripts/textesgb/eva15.html')
 soup = bs.BeautifulSoup(webpage.text, 'html.parser')
 page_text = soup.get_text()
 
 #------------------
 
 regex = re.compile(r"(?m).+:[^*#:]+\n")
-
 line = re.findall(regex,page_text)
 
 #------------------
 
-word_filter = ['Neon','Episode','Email','Nadia','http'] # list of words to filter out translator notes, using words not used in the actual scripts
+# list of words to filter out translator notes on each page
+word_filter = ['Neon','Episode','Email','Nadia','http','EVA','     ','NEON','EPISODE','Preview','4',]
 result = [x for x in line if not any(s in x for s in word_filter)] # list of each distinct line in the episode
-# result = [x for x in line if 'Neon' not in x] # list of each distinct line in the episode
-
 
 character_lines = {}
 
@@ -26,7 +24,8 @@ character_lines = {}
 # if character not already in character_lines, otherwise adding to the existing entry
 for text in result:
     character_name = re.findall('[^:#]*',text)[0] # regex to pull the name of the character speaking
-    word_count_line = len(text.split())-1
+    character_name = character_name.strip('!"#$%&(*+, ''-./:;<=>?@[\]^_`{|}~') # stripping leading + trailing punctuation
+    word_count_line = len(text.split())-1 # len-1 for initial name of character in line
     if character_name in character_lines:
         character_lines[character_name][0] += 1
         character_lines[character_name][1] += word_count_line
@@ -34,8 +33,6 @@ for text in result:
         character_lines[character_name] = [1,word_count_line]
 
 print(character_lines)
-print()
-print(result[0])
 
 # matches = re.finditer(regex, page_text)
 # for matchNum, match in enumerate(matches, start=1):
