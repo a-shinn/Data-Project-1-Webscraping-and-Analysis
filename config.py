4c186fd5d87b29bab6754c140cae17a78e6cfeab
Austin S <austinshinn99@gmail.com>
2023-06-14 15:12:55 -0500

    commit

---
+++ config.py
@@ -0,0 +1,12 @@
+# util.prompt_for_user_token(username='shinnsplints',
+#                                    scope='user-library-read',
+#                                    client_id='d4a6c5e1b1374d4a8356a82f68c9e64e',
+#                                    client_secret='ce3a391946574865a529e385548148a6',
+#                                    redirect_uri='https://localhost:8888/callback')
+
+USER='shinnsplints'
+CLIENT_ID = 'd4a6c5e1b1374d4a8356a82f68c9e64e'
+CLIENT_SECRET = 'ce3a391946574865a529e385548148a6'
+DIRECT_URI = 'https://localhost:8888/callback'
+
+PLAY_LIST = 'https://open.spotify.com/playlist/6RW2W9Gftoduqk6vm0SgGx?si=d2d4478fddff40aa'
